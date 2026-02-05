8ecfd993876a2aa2e6c0f2f7f67db3f3b570a7d1
galvanfran1234 <galvanfranco228@gmail.com>
2026-02-05 18:50:55 -0300

    Add files via upload

---
+++ veterinaria_patitas_felices.sql
@@ -0,0 +1,274 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.3
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: mysql:3306
+-- Tiempo de generación: 05-02-2026 a las 21:50:01
+-- Versión del servidor: 5.7.44
+-- Versión de PHP: 8.3.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `veterinaria_patitas_felices`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `duenos`
+--
+
+CREATE TABLE `duenos` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `telefono` varchar(20) NOT NULL,
+  `direccion` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `duenos`
+--
+
+INSERT INTO `duenos` (`id`, `nombre`, `apellido`, `telefono`, `direccion`) VALUES
+(1, 'Juan', 'Perez', '123456789', 'Nueva Dirección 123'),
+(2, 'Maria', 'Gomez', '987654321', 'Avenida Siempre Viva 456'),
+(3, 'Carlos', 'Lopez', '555666777', 'Boulevard Central 789');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `historial_clinico`
+--
+
+CREATE TABLE `historial_clinico` (
+  `id` int(11) NOT NULL,
+  `id_mascota` int(11) DEFAULT NULL,
+  `id_veterinario` int(11) DEFAULT NULL,
+  `fecha_registro` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `descripcion` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `historial_clinico`
+--
+
+INSERT INTO `historial_clinico` (`id`, `id_mascota`, `id_veterinario`, `fecha_registro`, `descripcion`) VALUES
+(1, 1, 1, '2025-12-12 21:09:54', 'Chequeo general, vacunación y desparasitación.'),
+(2, 2, 2, '2025-12-12 21:09:54', 'Cirugía menor para extracción de cuerpo extraño.');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mascotas`
+--
+
+CREATE TABLE `mascotas` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `especie` varchar(30) NOT NULL,
+  `fecha_nacimiento` date DEFAULT NULL,
+  `id_dueno` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `mascotas`
+--
+
+INSERT INTO `mascotas` (`id`, `nombre`, `especie`, `fecha_nacimiento`, `id_dueno`) VALUES
+(1, 'Firulais', 'Perro', '2018-05-10', 1),
+(2, 'Michi', 'Gato', '2020-08-15', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `roles`
+--
+
+INSERT INTO `roles` (`id`, `name`) VALUES
+(1, 'user'),
+(2, 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user_roles`
+--
+
+CREATE TABLE `user_roles` (
+  `user_id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `veterinarios`
+--
+
+CREATE TABLE `veterinarios` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `matricula` varchar(20) NOT NULL,
+  `especialidad` varchar(50) DEFAULT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `veterinarios`
+--
+
+INSERT INTO `veterinarios` (`id`, `nombre`, `apellido`, `matricula`, `especialidad`, `email`, `password`) VALUES
+(1, 'Ana', 'Martinez', 'VET12345', 'Dermatología', '0', '0'),
+(2, 'Luis', 'Rodriguez', 'VET67890', 'Cirugía', '0', '0');
+
+--
+-- Disparadores `veterinarios`
+--
+DELIMITER $$
+CREATE TRIGGER `assign_default_role_after_insert` AFTER INSERT ON `veterinarios` FOR EACH ROW BEGIN
+ 
+  DECLARE defaultRoleId INT;
+
+ 
+  SELECT id INTO defaultRoleId 
+  FROM roles 
+  WHERE name = 'user' 
+  LIMIT 1;
+
+ 
+  IF defaultRoleId IS NOT NULL THEN
+    INSERT INTO user_roles (user_id, role_id) 
+    VALUES (NEW.id, defaultRoleId);
+  END IF;
+END
+$$
+DELIMITER ;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `duenos`
+--
+ALTER TABLE `duenos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `historial_clinico`
+--
+ALTER TABLE `historial_clinico`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_mascota` (`id_mascota`),
+  ADD KEY `id_veterinario` (`id_veterinario`);
+
+--
+-- Indices de la tabla `mascotas`
+--
+ALTER TABLE `mascotas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_dueno` (`id_dueno`);
+
+--
+-- Indices de la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `user_roles`
+--
+ALTER TABLE `user_roles`
+  ADD PRIMARY KEY (`user_id`,`role_id`),
+  ADD KEY `role_id` (`role_id`);
+
+--
+-- Indices de la tabla `veterinarios`
+--
+ALTER TABLE `veterinarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `matricula` (`matricula`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `duenos`
+--
+ALTER TABLE `duenos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `historial_clinico`
+--
+ALTER TABLE `historial_clinico`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `mascotas`
+--
+ALTER TABLE `mascotas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `veterinarios`
+--
+ALTER TABLE `veterinarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `historial_clinico`
+--
+ALTER TABLE `historial_clinico`
+  ADD CONSTRAINT `historial_clinico_ibfk_1` FOREIGN KEY (`id_mascota`) REFERENCES `mascotas` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `historial_clinico_ibfk_2` FOREIGN KEY (`id_veterinario`) REFERENCES `veterinarios` (`id`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `mascotas`
+--
+ALTER TABLE `mascotas`
+  ADD CONSTRAINT `mascotas_ibfk_1` FOREIGN KEY (`id_dueno`) REFERENCES `duenos` (`id`);
+
+--
+-- Filtros para la tabla `user_roles`
+--
+ALTER TABLE `user_roles`
+  ADD CONSTRAINT `user_roles_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `veterinarios` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `user_roles_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
